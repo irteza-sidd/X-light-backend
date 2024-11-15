c9ee492598aeb2939237be2cbe433cf68c5b76d9
irteza-sidd <irtizasiddiqui18@gmail.com>
2024-11-15 17:31:13 +0500

    first commit setup

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const express= require('express');
+const dotenv=require('dotenv').config();
+
+const app = express();
+
+app.listen(3000,()=>{
+    console.log('server is running on PORT',process.env.PORT)
+})
